da2db68a1cc1f89919235e2bfd78471af6900173
gomtoo3 <gomtoo3@naver.com>
2021-11-23 18:46:33 +0900

    [21.11.23] java scrip 1day

---
+++ 1. core-js/1. 변수/3. quiz.js
@@ -0,0 +1,14 @@
+// 1번 문제
+var admin, myName;
+myName = 'john';
+admin = myName ;
+console.log(admin);
+
+// 2번 문제
+var ourLivingPlanet = '지구';
+var  currentLogName ;
+currentLogName= '홍길동'
+console.log(ourLivingPlanet);
+console.log(currentLogName);
+
+
